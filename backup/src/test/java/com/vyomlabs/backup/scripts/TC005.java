a5d2a3cc6c8bfbbba1a7fcd205472f5535e354f7
manis <manis@192.168.0.18>
2021-10-13 14:47:56 -0400

    Adding new Test case TC005

--- backup/src/test/java/com/vyomlabs/backup/scripts/TC005.java
+++ backup/src/test/java/com/vyomlabs/backup/scripts/TC005.java
@@ -1,26 +1,26 @@
 package com.vyomlabs.backup.scripts;
-
+//added new cases TC004
 import org.testng.Assert;
-import org.testng.annotations.Test;
 
 import com.vyomlabs.backup.pages.HomePage;
 import com.vyomlabs.backup.pages.MyStorePage;
 import com.vyomlabs.backup.pages.ProductDetailPage;
 import com.vyomlabs.backup.pages.orderDetailedPage;
 
-@Test()
-public class TC001  extends BaseTest{	
+public class TC005 extends BaseTest{
+
 	
-	public void testLogin()
+	public void testYellowColor5()
 	{
 		HomePage hp = new HomePage(driver, actionUtils);
 		MyStorePage mp = hp.clickOnTab("dresses");
 	    ProductDetailPage PDP=mp.ClickOnProduct("5");
 		PDP.increaseQuantity(2);
-		PDP.selectSize("M");  
-		PDP.selectColor("Black");
+		PDP.selectSize("L");  //Fixed it for size large
+		PDP.selectColor("Yellow");
 		PDP.addToKart();
 		orderDetailedPage ODP = PDP.proceedToCheckout();
 		Assert.assertEquals(ODP.verifyProductAvailable("5"), true);
 	}
 }
+
